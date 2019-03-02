48a51b14063b58812aa95472423bda1722764452
jajangtea <jajangtea@za2ng2509>
2019-03-02 07:02:16 +0700

    login admin masih error

--- backend/views/layouts/slider.php
+++ backend/views/layouts/slider.php
@@ -1,38 +1,51 @@
 <?php
 
+use yii\helpers\Html;
 use yii\helpers\Url;
 ?>
-<div class="sb-slidebar sb-right sb-style-overlay">
+<style type="text/css">
+    .modalB{
+        background: #111;
+        border-top: none;
+        border-bottom: solid 1px #2f2f2f;
+        border-left: solid 1px #2f2f2f;
+        text-decoration: none;
+        padding: 12px 20px;          
+        text-align: left;
+        width: 300px;
+        color: #ddd;
+    }
+    .modalB:hover{
+        background-color: #000000;
+    }
+</style>
+<div id="mySidenav" class="sidenav sb-style-overlay">
     <div class="input-group">
-        <a href="javascript:void(0);" class="sb-icon-navbar sb-toggle-right"><i class="fa fa-bars"></i></a>
+          <a href="javascript:void(0)" class="sb-icon-navbar sb-toggle-right" onclick="closeNav()">&times;</a>
     </div><!-- /input-group -->
 
     <h2 class="slidebar-header no-margin-bottom">Navigation</h2>
     <ul class="slidebar-menu">
         <li><a href="<?= Url::home() ?>">Home</a></li>
         <li><?php
-            if (Yii::$app->user->isGuest) 
-            {
-                echo "<a href=".Url::to(['site/login']).">Login</a>";
-                echo "<a href=".Url::to(['site/signup']).">Register</a>";
-            } else 
-            {
+            if (Yii::$app->user->isGuest) {
+                echo Html::button('Login', ['value' => Url::to(['site/login']), 'class' => 'modalB', 'id' => 'modalButton']);
+                echo Html::button('Register', ['value' => Url::to(['site/signup']), 'class' => 'modalB', 'id' => 'modalButtonRegister']);
+            } else {
                 echo '<a data-method="post" href="site/logout">Logout</a>';
             }
             ?>
         </li>
-        <li><a href="<?= Url::to(['site/signup']) ?>">Register</a></li>
         <li><a href="page_contact.html">Contact</a></li>
     </ul>
 
-    <h2 class="slidebar-header">Social Media</h2>
-    <div class="slidebar-social-icons">
-        <a href="#" class="social-icon-ar rss"><i class="fa fa-instagram"></i></a>
-        <a href="#" class="social-icon-ar facebook"><i class="fa fa-facebook"></i></a>
-        <a href="#" class="social-icon-ar twitter"><i class="fa fa-twitter"></i></a>
-        <a href="#" class="social-icon-ar wordpress"><i class="fa fa-wordpress"></i></a>
-        <a href="#" class="social-icon-ar linkedin"><i class="fa fa-youtube"></i></a>
-    </div>
+     <h2 class="slidebar-header no-margin-bottom">Social Media</h2>
+     <ul class="slidebar-menu">
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Instagram</a></li>
+        <li><a href="#">Youtube</a></li>
+        <li><a href="#">Twitter</a></li>
+    </ul>
 </div> <!-- sb-slidebar sb-right -->
 <div id="back-top">
     <a href="#header"><i class="fa fa-chevron-up"></i></a>
